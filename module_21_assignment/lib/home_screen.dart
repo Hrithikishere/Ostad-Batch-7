7e22d519fb35c4f7aa9741c81125086dbf0e8b94
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-12-11 10:41:27 +0600

    fixed

--- module_21_assignment/lib/home_screen.dart
+++ module_21_assignment/lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,21 +12,94 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   late GoogleMapController mapController;
   Set<Marker> _markers = {};
   Polyline _polyline = const Polyline(polylineId: PolylineId('path'));
   LatLng _currentLocation = LatLng(23.78829596078535, 90.42486190416521);
+  Position? position;
   Timer? _timer;
+
+  Future<void> listenCurrentLocation() async {
+    final isGranted = await isLocationPermissionGranted();
+    if (isGranted) {
+      final isServiceEnabled = await checkGPSServiceEnable();
+      if (isServiceEnabled) {
+        Geolocator.getPositionStream(
+                locationSettings: const LocationSettings(
+                    // timeLimit: Duration(seconds: 3),
+                    distanceFilter: 10,
+                    accuracy: LocationAccuracy.bestForNavigation))
+            .listen((pos) {
+          print(pos);
+        });
+      } else {
+        Geolocator.openLocationSettings();
+      }
+    } else {
+      final result = await requestLocationPermission();
+      if (result) {
+        getCurrentLocation();
+      } else {
+        Geolocator.openAppSettings();
+      }
+    }
+  }
+
+  Future<void> getCurrentLocation() async {
+    final isGranted = await isLocationPermissionGranted();
+    if (isGranted) {
+      final isServiceEnabled = await checkGPSServiceEnable();
+      if (isServiceEnabled) {
+        Position p = await Geolocator.getCurrentPosition();
+        position = p;
+        setState(() {});
+      } else {
+        Geolocator.openLocationSettings();
+      }
+    } else {
+      final result = await requestLocationPermission();
+      if (result) {
+        getCurrentLocation();
+      } else {
+        Geolocator.openAppSettings();
+      }
+    }
+  }
+
+  Future<bool> isLocationPermissionGranted() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> checkGPSServiceEnable() async {
+    return await Geolocator.isLocationServiceEnabled();
+  }
 
   @override
   void initState() {
     super.initState();
+    listenCurrentLocation();
     _getUserLocation();
   }
 
   void _getUserLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     setState(() {
       _currentLocation = LatLng(position.latitude, position.longitude);
       _markers.add(Marker(
@@ -33,17 +107,21 @@ class _HomeScreenState extends State<HomeScreen> {
         position: _currentLocation,
         infoWindow: InfoWindow(
           title: 'My current location',
-          snippet: 'Lat: ${_currentLocation.latitude}, Lng: ${_currentLocation.longitude}',
+          snippet:
+              'Lat: ${_currentLocation.latitude}, Lng: ${_currentLocation.longitude}',
         ),
       ));
     });
-    mapController.animateCamera(CameraUpdate.newLatLngZoom(_currentLocation, 15));
+    mapController
+        .animateCamera(CameraUpdate.newLatLngZoom(_currentLocation, 15));
 
-    _timer = Timer.periodic(Duration(seconds: 10), (Timer t) => _updateLocation());
+    _timer =
+        Timer.periodic(Duration(seconds: 10), (Timer t) => _updateLocation());
   }
 
   void _updateLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     setState(() {
       LatLng newLocation = LatLng(position.latitude, position.longitude);
       _markers.add(Marker(
@@ -51,7 +129,8 @@ class _HomeScreenState extends State<HomeScreen> {
         position: newLocation,
         infoWindow: InfoWindow(
           title: 'My current location',
-          snippet: 'Lat: ${newLocation.latitude}, Lng: ${newLocation.longitude}',
+          snippet:
+              'Lat: ${newLocation.latitude}, Lng: ${newLocation.longitude}',
         ),
       ));
       _polyline = Polyline(
